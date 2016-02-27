acf7d8780d6ce09dead86d4f5796c9ef6c88f830
Casey McGuire <casey.jay.mcguire@gmail.com>
2016-02-27 11:26:19 -0800

    Added some better commenting to code and added input file

--- Day15/Scala/Solution.scala
+++ Day15/Scala/Solution.scala
@@ -26,12 +26,24 @@ object Main {
     println(highestScoreOf500CalorieCookie)
   }
 
-  //Converts a sequence of Tuple2's (a,b) to a map where each key-value pair 
-  //is (b -> a)
+  /**
+    * Given a sequence of Tuple2's, returns a map where each key-value pair is 
+    * the Tuple2's second element mapped to the first.
+    * 
+    * @param coll The sequence to convert
+    */
   def reverseMap[A,B](coll: IndexedSeq[Tuple2[A,B]]): Map[B,A] = {
     coll.foldRight(Map[B,A]())((x,acc) => acc + (x._2 -> x._1))
   }
 
+  /**
+    * Given a map whose keys are ingredients and whose corresponding values are 
+    * the amount of each ingredient, return the total score of this combination 
+    * as laid out in the problem.
+    * 
+    * @param ingredients The ingredient combination of the this cookie
+    * @return The score of this combination of ingredients
+    */
   def getTotalScore(ingredients: Map[Ingredient, Int]): Long = {
     val ingredientFields =
       List(
@@ -48,18 +60,35 @@ object Main {
     })
   }
 
+  /**
+    * Returns the score of an ingredient for a particular property.
+    * 
+    * @param ingredients A map whose keys are the ingredient and whose corresponding value
+    *                    is the amount of that ingredient
+    * @param f A function to apply to each ingredient in the map. This is intended to pull
+    *          out what property we're currently calculating the score for.
+    * @return The total score of all the ingredient for the particular property.
+    */
   private def getScore(ingredients: Map[Ingredient, Int], f: (Ingredient => Int)): Long =
     ingredients.foldRight(0L)((keyVal, acc) => {
       val (property, ingredientAmount) = keyVal
       f(property) * ingredientAmount + acc
     })
 
-
+  /**
+    * Calculate the total calories of the given ingredients.
+    * 
+    * @param ingredients A map whose keys are the ingredient and whose corresponding value
+    *                     is the amount of that ingredient
+    * @return The total number of calories in the given cookie.
+    */
   def getTotalCalories(ingredients: Map[Ingredient, Int]): Long = 
     getScore(ingredients, _.calories)
 
 
-
+  /**
+    * Parses each line of the input file into an ingredient.
+    */
   private def parseLineToIngredient(line: String): Ingredient = {
     line.split(" ") match {
       case Array(name, _, capacity, _, durability, _, flavor, _, texture, _, calories) => {
