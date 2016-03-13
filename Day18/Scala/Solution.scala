c080c0b999ec342318a1537c947e0574f00cbcec
Casey McGuire <casey.jay.mcguire@gmail.com>
2016-03-12 22:40:50 -0800

    Finished Day 18 in Scala

--- Day18/Scala/Solution.scala
+++ Day18/Scala/Solution.scala
@@ -4,44 +4,61 @@ object Main {
     val lines = scala.io.Source.fromFile(args(0)).getLines.toArray
     val lights = lines.map(_.toCharArray)
 
-    println(getNumLightsOnAfterNumSteps(lights, 100))
+    //part 1
+    println(getNumLightsOnAfterNumSteps(lights, 100, false))
+
+    //part 2
+    println(getNumLightsOnAfterNumSteps(lights, 100, true))
   }
 
-  def getNumLightsOnAfterNumSteps(lights: Array[Array[Char]], steps: Int): Int = {
+  def getNumLightsOnAfterNumSteps(lights: Array[Array[Char]], steps: Int, cornerLightsAlwaysOn: Boolean): Int = {
     @scala.annotation.tailrec
     def getNumLightsOnAfterNumSteps(curStep: Int, lights: Array[Array[Char]]): Int = {
       if(curStep == steps) getNumLightsOn(lights)
       else {
         val nextConfiguration = getNextConfiguration(lights)
-        getNumLightsOnAfterNumSteps(curStep + 1, lights)
+        getNumLightsOnAfterNumSteps(curStep + 1, nextConfiguration)
       }
     }
 
+    /**
+      * Returns the number of lights that are on in the passed light grid.
+      */
     def getNumLightsOn(lights: Array[Array[Char]]): Int = {
-      var sum = 0
-      for(i <- 0 until lights.size) {
-        for(j <- 0 until lights(i).size) {
-          if(lights(i)(j) == '#') sum += 1
+      lights.map(_.foldRight(0)((elem, acc) => {
+        elem match {
+          case '#' => acc + 1
+          case _ => acc
         }
-      }
-      sum
+      })).sum
     }
 
+    /**
+      * Returns the next light grid given the state of the current light grid.
+      */
     def getNextConfiguration(lights: Array[Array[Char]]): Array[Array[Char]] = {
       val newLights = Array.ofDim[Char](lights.size, lights(0).size)
-      for(i <- 0 until lights.size) {
-        for(j <- 0 until lights(0).size) {
+       (0 until lights.size).foreach(i => {
+         (0 until lights(i).size).foreach(j => {
           newLights(i)(j) = getNextLight(i, j, lights)
-        }
+        })
+      })
 
-      }
-
+      /**
+        * Returns the next state of the light at the passed coordinate.
+        * 
+        * @param x The x-coordinate in the grid
+        * @param y The y-coordinate in the grid
+        * @param lights The current light grid
+        * @return '#' if the light will be on; '.' if the light will be off
+        */
       def getNextLight(x: Int, y: Int, lights: Array[Array[Char]]): Char = {
-        var sum = 0
+        var numLitNeighbors = 0
+
         for(i <- -1 to 1) {
           for(j <- -1 to 1) {
-            val numLightsAtSpot = 
-            if(i == 0 && j == 0) {
+            val numLightsAtSpot =
+              if(i == 0 && j == 0) {
                 0
               } else {
                 (i + x, j + y) match {
@@ -52,19 +69,34 @@ object Main {
                   case (newX, newY) => if(lights(newX)(newY) == '#') 1 else 0
                 }
               }
-            sum += numLightsAtSpot
+            numLitNeighbors += numLightsAtSpot
           }
         }
+
         val isLightOn = lights(x)(y) == '#'
-        if(isLightOn) {
-          if(sum == 2 || sum == 3)  '#'
-          else                      '.'
+
+        /** @return True if the passed coordinates represent a corner in the board */
+        val isCornerLight: (Int, Int) => Boolean = (i, j) => {
+          (i, j) match  {
+            case (0, 0) => true
+            case (0, 99) => true
+            case (99, 0) => true
+            case (99, 99) => true
+            case _ => false
+          }
+        }
+
+        if(cornerLightsAlwaysOn && isCornerLight(x, y)) {
+          '#'
+        } 
+        else if(isLightOn) {
+          if(numLitNeighbors == 2 || numLitNeighbors == 3) '#'
+          else                                             '.'
         } else {
-          if(sum == 3) '#'
-          else         '.'
+          if(numLitNeighbors == 3) '#'
+          else                     '.'
         }
       }
-
       newLights
     }
     getNumLightsOnAfterNumSteps(0, lights)
