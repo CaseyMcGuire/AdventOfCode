0557a0a69da4a7a639f0ee2dc1d33aba1cc8acc6
Casey McGuire <casey.jay.mcguire@gmail.com>
2016-03-13 17:51:26 -0700

    First part 1 of Day 19

---
+++ Day19/Scala/Solution.scala
@@ -0,0 +1,50 @@
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val lines = scala.io.Source.fromFile(args(0)).getLines.toArray
+    val (replacements, inputs) = lines.span(!_.isEmpty)
+    val (replacementRules, input) = (parseLines(replacements), inputs(1))
+    println(getNumDistinctMolecules(replacementRules, input))
+  }
+
+  def getNumDistinctMolecules(replacementRules: Map[String, List[String]], input: String): Int = {
+    
+    def getDistinctMolecules(passedChars: Array[String], remainingChars: List[String], acc: Set[String]): Set[String] = {
+      remainingChars match {          
+        case x::y::xs => {
+          val newPassedChars = passedChars :+ x
+          val a = getMolecules(passedChars, x, y::xs)
+          val b = getMolecules(passedChars :+ x, y, xs)
+          val c = getMolecules(passedChars, x + y, xs)
+          getDistinctMolecules(newPassedChars, y::xs, acc ++ a ++ b ++ c)
+        }
+        case x::Nil => {
+          val newPassedChars = passedChars :+ x
+          val a = getMolecules(passedChars, x, List[String]())
+          getDistinctMolecules(newPassedChars, Nil, a ++ acc)
+        }
+        case Nil => acc
+      }
+    }
+
+    def getMolecules(passedChars: Array[String], curRule: String, remainingChars: List[String]): Set[String] = {
+      val mappings = replacementRules.getOrElse(curRule, List[String]())
+      mappings.foldRight(Set[String]())((elem, acc) => acc + (passedChars.mkString + elem.mkString + remainingChars.mkString))
+    }
+
+    getDistinctMolecules(Array[String](), input.split("").toList, Set[String]()).size
+  }
+
+
+  private def parseLines(lines: Array[String]): Map[String, List[String]] = {
+    lines.foldLeft(Map[String, List[String]]())((acc, line) => {
+      line.split(" ") match {
+        case Array(input, _, generation) => {
+          val existingRules = acc.getOrElse(input, List[String]())
+          acc + (input -> (generation :: existingRules))
+        }
+      }
+    })
+  }
+}
